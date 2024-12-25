5cb3880d6d7880d21cadca36fb4a9335c90164c7
Bsm21219 <basma.hina19@gmail.com>
2024-12-25 21:38:01 +0100

    Update euclide_etendu.py

--- euclide_etendu.py
+++ euclide_etendu.py
@@ -1,14 +1,22 @@
 def egcd(a, b):
-    x,y, u,v = 0,1, 1,0
+    """
+    Algorithme d'Euclide étendu pour calculer le PGCD  de deux nombres,
+    ainsi que les coefficients de la relation de Bézout.
+    """
+    x,y, u,v = 0,1, 1,0  # coefficients initiaux
     while a != 0:
-        q, r = b//a, b%a
-        m, n = x-u*q, y-v*q
-        b,a, x,y, u,v = a,r, u,v, m,n
+        q, r = b//a, b%a  # quotient et reste
+        m, n = x-u*q, y-v*q # mise à jour des coefficients
+        b,a, x,y, u,v = a,r, u,v, m,n # mise à jour des variables
     gcd = b
     return gcd, x, y
 
 def modinv(a, m):
+    """
+    Calcule de l'inverse modulaire de 'a' modulo 'm' en utilisant l'algorithme
+    d'Euclide étendu. Si l'inverse n'existe pas, la fonction retourne 'None'.
+    """
     gcd, x, y = egcd(a, m)
     if gcd != 1:
-        return None
-    return x % m
+        raise ValueError(f"L'inverse modulaire n'existe pas pour {a} et {m}.")
+    return x % m # Retourne l'inverse modulaire
