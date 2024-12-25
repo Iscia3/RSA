72e989f34cfa0086247c488fdb1b9779d90af0ba
Bsm21219 <basma.hina19@gmail.com>
2024-12-26 00:51:44 +0100

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -4,8 +4,8 @@ import lpowmod
 e, d, n=rsa_key.gen_rsa_keys(10) # Générer les clés RSA (taille de 10 chiffres pour p et q)
 
 #Fonction de chiffrement
-def encrypt(public_key: tuple[int,int], message: bytes) -> list[int]: # la clée public est un paramètre explicite
-    e, n = public_key # tuple (e,n) pour gérer proprement la clée publique
+def encrypt(public_key: couple[int,int], message: bytes) -> list[int]: # la clée public est un paramètre explicite
+    e, n = public_key # couple (e,n) pour gérer proprement la clée publique
     encrypted= []
     for b in message:
         encrypted.append(lpowmod.lpowmod(int(b),e,n))
@@ -13,7 +13,7 @@ def encrypt(public_key: tuple[int,int], message: bytes) -> list[int]: # la clée
     
 #Fonction de déchiffrement
 def decrypt(private_key: tuple[int,int], encrypted_message:list[int]) -> bytes: # la clée privée est un paramètre explicite
-    d, n = private_key # tuple (d,n) pour gérer proprement la clée privée
+    d, n = private_key # couple (d,n) pour gérer proprement la clée privée
     decrypted=b""
     for b in encrypted_message:
         decrypted_byte = lpowmod.lpowmod(b, d , n)
