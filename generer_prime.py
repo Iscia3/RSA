8f332764b674398f81d46e424ae7ff830202d450
Bsm21219 <basma.hina19@gmail.com>
2024-12-25 23:53:33 +0100

    Update generer_prime.py

--- generer_prime.py
+++ generer_prime.py
@@ -2,27 +2,37 @@ import random
 import premier 
 
 def gen_prime(length):
+    """
+    Génère un nombre premier de 'lenght' chiffres.
+    Arguments:
+    -lenght (int) : Nombre de chiffres du nombre premier à générer.
+    Retourne:
+    - (int) : Un nombre premier aléatoire de 'lenght' chiffres.
+    """
     random.seed()
-    first_digit="123456789"
-    digit="0"+first_digit
-    last_digit="1379"
-    nb=""
-    nb_premier=""
-    for i in range(length):
-        if i==0:
-            nb=nb+random.choice(first_digit)
-        elif i==length-1:
-            nb=nb+random.choice(last_digit)
-        else:
-            nb=nb+random.choice(digit)
-    #print(nb)
 
-    if premier.is_prime(nb)==True:
-        nb_premier=nb
-    else:
-        return gen_prime(length)
+    while True:
+        # Construction du nombre aléatoire
+        first_digit=random.choice("123456789") # Premier chiffre
+        digit="0"+first_digit
+        last_digit=random.choice("1379") # dernier chiffre
+        nb=""
+        nb_premier=""
+        for i in range(length):
+            if i==0:
+                nb=nb+random.choice(first_digit)
+            elif i==length-1:
+                nb=nb+random.choice(last_digit)
+            else:
+                nb=nb+random.choice(digit)
+            #print(nb)
+
+            if premier.is_prime(nb)==True:
+                nb_premier=nb
+            else:
+                return gen_prime(length)
     
-    return nb_premier
+      return nb_premier
 
 
 #print(gen_prime(2000))
