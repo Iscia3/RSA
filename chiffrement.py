8ff636b1a80ae71a2b62c3c0abba6eef80278b9a
Bsm21219 <basma.hina19@gmail.com>
2024-12-25 17:51:08 +0100

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -1,26 +1,38 @@
 import rsa_key
 import lpowmod
 
-e, d, n=rsa_key.gen_rsa_keys(10)
+e, d, n=rsa_key.gen_rsa_keys(10) # Générer les clés RSA (taille de 10 chiffres pour p et q)
 
-
-def encrypt(public_key, message :bytes) -> list[int]:
-    L=[]
+#Fonction de chiffrement
+def encrypt(public_key: tuple[int,int], message: bytes) -> list[int]: # la clée public est un paramètre explicite
+    e, n = public_key
+    encrypted= []
     for b in message:
-        L.append(lpowmod.lpowmod(int(b),e,n))
-    return L
+        encrypted.append(lpowmod.lpowmod(int(b),e,n))
+    return encrypted
+    
+#Fonction de déchiffrement
+def decrypt(private_key: tuple[int,int], encrypted_message:list[int]) -> bytes: # la clée privée est un paramètre explicite
+    d, n = private_key
+    decrypted=b""
+    for b in encrypted_message:
+        decrypted_byte = lpowmod.lpowmod(b, d , n)
+        byte_size = (decrypted_byte.bit_lenght() + 7 // 8  # Taille en octets, la taille des octets déchiffrés est calculée dynamiquement avec bit_lenght
+        decrypted += decrypted_byte.to_bytes(byte_size, "big")
+    return decrypted
+# Message à chiffrer
+message = "bonjour" # le message "bonjour" sera chiffré en une liste de nombres
 
-def decrypt(private_key, message_crypted:list[int]) -> bytes:
-    result=b""
-    for b in message_crypted:
-      B=lpowmod.lpowmod(b,d,n)
-      byte = B.to_bytes(1,"little")
-      result+=byte
-    return result
+#Chiffrement et déchiffrement
+public_key = (e,n)
+private_key = (d,n)
 
-mot="bonjour"       
-code=decrypt(d,encrypt(e,mot.encode("utf-8")))
-print(code.decode("utf-8"))
+encrypted_message = encrypt(public_key, message.encode("utf-8"))
+decrypted_message = decrypt(private_key, ecrypted_message) # le déchiffrement permettra de retrouver le texte d'origine
+
+print("Message initial: ", message)
+print("Message chiffré: ", ecrypted_message)
+print("Message déchiffré: ", decrypted_message.decode("utf-8"))
 
 
 
